d0712abb677b39faf342ec094eedbe83df2fb204
cliffhall <cliff@futurescale.com>
2021-03-17 15:44:44 -0400

    WIP: Refactor to Diamond standard
    * In deploy-diamond.js,
      - refactor the contract deployments to build cut objects to pass to the CannonFacet's constructor
      - add configuration and deployment of CannonFacet
        - must manually remove the selector for supportsInterface() method which which is already deployed by the DiamondLoupeFacet

--- scripts/deploy-diamond.js
+++ scripts/deploy-diamond.js
@@ -7,64 +7,76 @@ const FacetCutAction = {
 }
 
 function getSelectors (contract) {
-    const signatures = Object.keys(contract.interface.functions)
+    const signatures = Object.keys(contract.interface.functions);
     const selectors = signatures.reduce((acc, val) => {
         if (val !== 'init(bytes)') {
             acc.push(contract.interface.getSighash(val))
         }
         return acc
-    }, [])
-    return selectors
+    }, []);
+    return selectors.reverse();
+}
+
+function removeItem (array, item) {
+    array.splice(array.indexOf(item), 1)
+    return array
 }
 
 async function main() {
 
-    const accounts = await hre.ethers.provider.listAccounts();
+    const accounts = await ethers.provider.listAccounts();
     const deployer = accounts[0];
     console.log("Deployer account: ", deployer ? deployer : "not found" && process.exit() );
 
-    // Deploy the Test, Cut, Loupe, Ownership, and Diamond facets
+    // Deploy the Cannnon, Cut, Loupe, Ownership, and Diamond facets
 
-    // Test 1 Facet
-    const Test1Facet = await hre.ethers.getContractFactory("Test1Facet");
-    const test1Facet = await Test1Facet.deploy();
-    await test1Facet.deployed();
-    console.log("Test1Facet deployed to:", test1Facet.address);
-
-    // Test 2
-    const Test2Facet = await hre.ethers.getContractFactory("Test2Facet");
-    const test2Facet = await Test2Facet.deploy();
-    await test2Facet.deployed();
-    console.log("Test2Facet deployed to:", test2Facet.address);
+    // Cannon Facet
+    const CannonFacet = await ethers.getContractFactory("CannonFacet");
+    const ncf = await CannonFacet.deploy();
+    await ncf.deployed();
+    const cannonSelectors = getSelectors(ncf);
+    removeItem(cannonSelectors, '0x01ffc9a7'); // EIP-165 supportsInterface() exists in
+    const cannonCut = [ncf.address, FacetCutAction.Add, cannonSelectors];
+    console.log("CannonFacet deployed to:", ncf.address);
 
     // Diamond Cut Facet
-    const DiamondCutFacet = await hre.ethers.getContractFactory("DiamondCutFacet");
-    const diamondCutFacet = await DiamondCutFacet.deploy();
-    await diamondCutFacet.deployed();
-    console.log("DiamondCutFacet deployed to:", diamondCutFacet.address);
+    const DiamondCutFacet = await ethers.getContractFactory("DiamondCutFacet");
+    const dcf = await DiamondCutFacet.deploy();
+    await dcf.deployed();
+    const diamondCutSelectors = getSelectors(dcf);
+    const diamondCutCut = [dcf.address, FacetCutAction.Add, diamondCutSelectors];
+    console.log("DiamondCutFacet deployed to:", dcf.address);
 
     // Diamond Loupe Facet
-    const DiamondLoupeFacet = await hre.ethers.getContractFactory("DiamondLoupeFacet");
-    const diamondLoupeFacet = await DiamondLoupeFacet.deploy();
-    await diamondLoupeFacet.deployed();
-    console.log("DiamondLoupeFacet deployed to:", diamondLoupeFacet.address);
+    const DiamondLoupeFacet = await ethers.getContractFactory("DiamondLoupeFacet");
+    const dlf = await DiamondLoupeFacet.deploy();
+    await dlf.deployed();
+    const diamondLoupeSelectors = getSelectors(dlf);
+    const diamondLoupeCut = [dlf.address, FacetCutAction.Add, diamondLoupeSelectors];
+    console.log("DiamondLoupeFacet deployed to:", dlf.address);
 
     // Ownership Facet
-    const OwnershipFacet = await hre.ethers.getContractFactory("OwnershipFacet");
-    const ownershipFacet = await OwnershipFacet.deploy();
-    await ownershipFacet.deployed().then(async () => {
+    const OwnershipFacet = await ethers.getContractFactory("OwnershipFacet");
+    const osf = await OwnershipFacet.deploy();
+    const ownershipSelectors = getSelectors(osf);
+    const ownershipCut = [osf.address, FacetCutAction.Add, ownershipSelectors];
+    await osf.deployed().then(async () => {
+
+        console.log("OwnershipFacet deployed to:", osf.address);
+
+        // Deploy Diamond with Cut, Loupe, Ownership, and Cannon facets pre-cut
         const diamondCut = [
-            [diamondCutFacet.address, FacetCutAction.Add, getSelectors(diamondCutFacet)],
-            [diamondLoupeFacet.address, FacetCutAction.Add, getSelectors(diamondLoupeFacet)],
-            [ownershipFacet.address, FacetCutAction.Add, getSelectors(ownershipFacet)]
+            diamondCutCut,
+            diamondLoupeCut,
+            ownershipCut,
+            cannonCut,
         ]
-        const Diamond = await hre.ethers.getContractFactory("Diamond");
-        const diamond = await Diamond.deploy(diamondCut, [deployer]);
-        await diamond.deployed();
-        console.log("Diamond deployed to:", diamond.address);
+        const Cannon = await ethers.getContractFactory("Diamond");
+        const cannon = await Cannon.deploy(diamondCut, [deployer]);
+        await cannon.deployed();
+
+        console.log("Diamond deployed to:", cannon.address);
     });
-    console.log("OwnershipFacet deployed to:", ownershipFacet.address);
-    console.log("\n");
 }
 
 main()
